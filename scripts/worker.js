eab474ca471007743af0b1cf6f4d4c97a7625a4a
etokheim <e@tokheim.no>
2016-11-30 15:05:35 +0100

    FEATURE: Add web-worker to do the heavy lifting + optimize functions to run faster

--- scripts/worker.js
+++ scripts/worker.js
@@ -5,23 +5,25 @@ this.onmessage = function(e) {
   var type = e.data.type;
 
   try {
+    var date = Date.now();
     length = imageData.data.length / 4;
+    var manipulate = determineFunction(type);
     for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
       r = imageData.data[i * 4 + 0];
       g = imageData.data[i * 4 + 1];
       b = imageData.data[i * 4 + 2];
       a = imageData.data[i * 4 + 3];
-      pixel = manipulate(type, r, g, b, a);
+      pixel = manipulate(r, g, b, a);
       imageData.data[i * 4 + 0] = pixel[0];
       imageData.data[i * 4 + 1] = pixel[1];
       imageData.data[i * 4 + 2] = pixel[2];
       imageData.data[i * 4 + 3] = pixel[3];
     }
-    postMessage(imageData);
+    postMessage([imageData, date]);
   } catch (e) {
     function ManipulationException(message) {
       this.name = "ManipulationException";
-      this.message = message;
+      this.message = message + " + e = " + e;
     };
     throw new ManipulationException('Image manipulation error');
     postMessage(undefined);
